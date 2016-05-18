e31cdeb17248d36aada26145e74c16ffa0824e84
平野一生 <hiranoi@pro.local>
2016-05-18 20:50:20 +0900

    TOPページに運用情報を追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514130828) do
+ActiveRecord::Schema.define(version: 20160514142043) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 20160514130828) do
     t.integer  "isbn",       limit: 8
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

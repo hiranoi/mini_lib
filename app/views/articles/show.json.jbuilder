1df95772768a61950fbfe646adc77dfd009afa1e
平野一生 <hiranoi@pro.local>
2016-06-25 17:51:52 +0900

    記事一覧機能の追加

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :url, :owner, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621141120) do
+ActiveRecord::Schema.define(version: 20160621143802) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20160621141120) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.string   "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
@@ -54,7 +62,6 @@ ActiveRecord::Schema.define(version: 20160621141120) do
     t.integer  "isbn",       limit: 8
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
-    t.integer  "owner_id"
     t.string   "owner"
   end
 
@@ -69,14 +76,6 @@ ActiveRecord::Schema.define(version: 20160621141120) do
   create_table "rent_histories", force: :cascade do |t|
     t.string   "name"
     t.string   "book"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "book_id"
-    t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

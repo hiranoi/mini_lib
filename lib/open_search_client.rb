0e9ce691aa14588b0f93ebde74d6190576a7c306
inouekoichi <inouekoichi01@gmail.com>
2016-10-29 11:55:41 +0900

    slideshareAPI通信部作成

--- lib/open_search_client.rb
+++ lib/open_search_client.rb
@@ -4,17 +4,33 @@ require 'rexml/document'
 # open_search_client.rb
 class OpenSearchClient
 
-  URL_HEAD = 'http://iss.ndl.go.jp'
-
   def get_book_info_by_isbn(isbn)
-    response = get('/api/opensearch', {:isbn => isbn})
+    response = get('http://iss.ndl.go.jp','/api/opensearch', {:isbn => isbn})
+    res_xml = REXML::Document.new(response.body)
+  end
+
+  def get_slideshow_by_url(url)
+
+    api_key = "BXUZhtwB"
+    secret_key = "zIZsdVoU"
+    now_time = Time.now.to_i
+    hash = Digest::SHA1.hexdigest("#{secret_key}#{now_time}")
+
+    pram = {
+      :api_key => api_key,
+      :ts => now_time,
+      :hash => hash,
+      :slideshow_url => url
+    }
+
+    response = get('https://www.slideshare.net', '/api/2/get_slideshow', pram)
     res_xml = REXML::Document.new(response.body)
   end
 
   private
 
-  def get(path, pram)
-    con = Faraday.new(:url => URL_HEAD) do |f|
+  def get(domain, path, pram)
+    con = Faraday.new(:url => domain) do |f|
       f.request  :url_encoded
       f.response :logger
       f.adapter  Faraday.default_adapter

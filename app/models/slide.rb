d320b6f041fb7db5faf7f27b00e39cd9ac7fe67b
inouekoichi <inouekoichi01@gmail.com>
2016-10-31 10:08:03 +0900

    通信部の修正

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -3,25 +3,11 @@ require 'nokogiri'
 class Slide < ActiveRecord::Base
 
   def self.inquiry_slide_detail(url)
-    
+
     res = OpenSearchClient.new.get_slideshow_by_url(url)
-    res_xml = res.elements
+    res_hash = Hash.from_xml(res.elements['//Slideshow'].to_s)
 
-    slide = Slide.new()
-
-    slide.slide_id = res_xml['//ID'].text
-    slide.title = res_xml['//Title'].text
-    slide.url = res_xml['//URL'].text
-    slide.thumbnail_url = res_xml['//ThumbnailURL'].text
-    slide.embed = res_xml['//Embed'].text
-
-    logger.debug("**** res_xml start ****")
-    logger.debug(slide.slide_id)
-    logger.debug(slide.title)
-    logger.debug(slide.url)
-    logger.debug(slide.thumbnail_url)
-    logger.debug(slide.embed)
-    logger.debug("**** res_xml end ****")
+    slide = parseFromHash(res_hash.fetch("Slideshow"))
 
     slide
   end

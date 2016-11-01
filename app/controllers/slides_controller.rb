fe6c759fb22a2c812b67a6aedd34b257f85c8147
inouekoichi <inouekoichi01@gmail.com>
2016-11-02 08:50:50 +0900

    スライド登録機能実装

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -9,11 +9,19 @@ class SlidesController < ApplicationController
 
 
   def new
-	@slide = Slide.new
+	   @slide = Slide.new
   end
 
   def create
-  	
+
+    url = params[:slide][:url]
+  	@slide = Slide.inquiry_slide_detail(url)
+
+    if @slide.save
+        redirect_to new_slide_path, notice: 'スライドをを登録しました。'
+    else
+        render :new
+    end
   end
 
 end

a8aac2d9838bf0f8deb73956b0999dd300956dab
inouekoichi <inouekoichi01@gmail.com>
2016-11-17 21:50:29 +0900

    仮実装

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,5 +1,5 @@
 class FeelingsController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   # TODO:実装中
   def index
@@ -8,6 +8,13 @@ class FeelingsController < ApplicationController
   end
 
   def create
-  	@feeling = Feeling.new(feeling_params)
+  	@feeling = Feeling.new()
+  	@feeling.target = params[:target]
+  	@feeling.target_id = params[:target_id]
+  	@feeling.feeling = params[:feeling]
+
+  	@feeling.save
+
+  	render :json => "true"
   end
 end

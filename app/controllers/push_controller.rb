861facc7dfad41297f8ac96541a04145dfcd20c9
inouekoichi <inouekoichi01@gmail.com>
2016-12-18 19:38:25 +0900

    連続で送れないようチェックを実装

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -2,14 +2,28 @@ class PushController < ApplicationController
 
   def create
 
+    # check sent push
+    if !Push.sent_check("news").empty?
+      render :json => "Push通知は1日1回までです"
+      return
+    end
+
     # get new articles
     articles = Article.get_new_articles
 
+    if articles.empty?
+      render :json => "新しい記事はありません"
+      return
+    end
+
+    # create push body
     push_body = ""
-    articles.each do |article|
+    articles.each_with_index do |article, i|
       push_body += "・"
       push_body += article.title
       push_body += "\n"
+
+      break if i == 2
     end
 
     # send push
--- app/models/push.rb
+++ app/models/push.rb
@@ -1,15 +1,25 @@
 class Push < ActiveRecord::Base
 
+  def self.sent_check(type)
+
+    t = Time.now
+
+    from = Time.local(t.year, t.month, t.day)
+    to = Time.local(t.year, t.month, t.day + 1)
+
+    Push.where(:type => type, :created_at => from..to)
+  end
+
   def self.send_news(push_body)
 
-    @push = Push.new
-    @push.type = 'news'
-    @push.title = '【JaQ News】新しい記事が追加されました'
-    @push.text = push_body
-    @push.save
+    push = Push.new
+    push.type = 'news'
+    push.title = '【JaQ News】新しい記事が追加されました'
+    push.text = push_body
 
-    OpenSearchClient.new.send_push(@push.title, @push.text)
-    @push
+    OpenSearchClient.new.send_push(push.title, push.text)
+
+    push.save
   end
 
 end

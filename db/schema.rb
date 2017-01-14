1ce7ebafeb7b187594c68714c0f001845311f75b
inouekoichi <inouekoichi01@gmail.com>
2017-01-14 10:52:48 +0900

    ニックネーム機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107035901) do
+ActiveRecord::Schema.define(version: 20170114012342) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -128,9 +128,9 @@ ActiveRecord::Schema.define(version: 20170107035901) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.boolean  "syoko"
     t.string   "place"
     t.boolean  "admin"
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

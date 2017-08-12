45ae0372dff1c63d59663e5f49a4b41347b38f12
inouekoichi <inouekoichi01@gmail.com>
2017-08-12 22:36:28 +0900

    GCP Postgres対応

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20170122073016) do
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -43,8 +31,8 @@ ActiveRecord::Schema.define(version: 20170122073016) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -111,7 +99,6 @@ ActiveRecord::Schema.define(version: 20170122073016) do
     t.string   "url"
     t.string   "thumbnail_url"
     t.string   "embed"
-    t.string   "recommend_user"
     t.string   "recommend_comment"
     t.integer  "user_id"
     t.datetime "created_at",        null: false
@@ -136,7 +123,7 @@ ActiveRecord::Schema.define(version: 20170122073016) do
     t.string   "user_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
